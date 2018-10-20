f2a7ea6d28960f911a183d18e66b2f00acf20bfc
vinchevskaya <winchewskaja@gmail.com>
2018-10-20 19:12:58 +0300

    Laborartory Work

---
+++ LaboratoryTask-1/LaboratoryTask-1/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace LaboratoryTask_1
+{
+    /// <summary>
+    /// This class represents every element in the stack.
+    /// </summary>
+    public class Node
+    {
+        public Node Next { get; set; }
+        public int Size { get; set; }
+        public Node head, tail;
+    }
+}
---
+++ LaboratoryTask-1/LaboratoryTask-1/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LaboratoryTask_1
+{
+    /// <summary>
+    /// Based on the Node class, we implement the stack structure.
+    /// </summary>
+    public class Stack : Node
+    {
+        public int StackContents { get; private set; }
+        public void Push(int value)
+        {
+            Node node = new Node { Size = value };
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                head = node.Next;
+                tail = node;
+            }
+            StackContents++;
+        }
+        public int Pop()
+        {
+            if (StackContents < 1)
+            {
+                throw new Exception("Stack is empty");
+            }
+            if (StackContents == 1)
+            {
+                StackContents = 0;
+                int count = head.Size;
+                head = tail = null;
+                return count;
+            }
+            Node index = head;
+            while (index.Next != tail)
+            {
+                index = index.Next;
+            }
+            int top = index.Next.Size;
+            tail = index;
+            StackContents--;
+            return top;
+        }
+    }
+}
