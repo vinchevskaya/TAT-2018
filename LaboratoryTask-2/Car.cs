8a88e03713bfed2da45bb16f8462e225b5cb0d3b
vinchevskaya <winchewskaja@gmail.com>
2018-10-27 15:35:04 +0300

    Sorting the list of cars by color

---
+++ LaboratoryTask-2/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace LaboratoryTask_2
+{
+    /// <summary>
+    /// Class constructor.
+    /// </summary>
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public Car(string model, string brand, string color)
+        {
+            this.Model = model;
+            this.Brand = brand;
+            this.Color = color;
+        }
+        public override string ToString()
+        {
+            return "Model: " + Model + " Brand: " + Brand + " Color: " + Color;
+        }
+    }
+}
