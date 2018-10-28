ef2311a0b2ceb235edd5fe158002ca22b979d485
vinchevskaya <winchewskaja@gmail.com>
2018-10-28 15:32:41 +0300

    Task 4

---
+++ DEV-4/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DEV_4
+{
+    /// <summary>
+    /// This class contains objects for the XML document.
+    /// </summary>
+    public class Node
+    {
+        private string tag;
+        private string value;
+        private string attributes;
+        private string attributeValue;
+        private List<Node> childrenNodes = new List<Node>();
+
+        public string Tag { get { return tag; } set { tag = value; } }
+
+        public string Value { get { return value; } set { this.value = value; } }
+
+        public string Attributes { get { return attributes; } set { attributes = value; } }
+
+        public string AttributeValue { get { return attributeValue; } set { attributeValue = value; } }
+
+        public List<Node> ChildrenNodes { get { return childrenNodes; } set { childrenNodes = value; } }
+    }
+}
---
+++ DEV-4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DEV_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathXMLFile = args[0];
+            XMLParser parser = new XMLParser();
+            string XMLFile = parser.ReadXMLFile(pathXMLFile);
+            Node node = new Node();
+            node = parser.ParseXMLFile(XMLFile);
+            OutputNode outputNode = new OutputNode();
+            outputNode.PrintNode(node);
+        }
+    }
+}
