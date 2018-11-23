f3f988eb504542b89d117158e201bd5637faac26
vinchevskaya <winchewskaja@gmail.com>
2018-11-23 16:41:52 +0300

    Task 7

---
+++ DEV-7/DEV-3/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DEV_3
+{
+    /// <summary>
+    /// This is еру entry point to the program.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConverterOfNumber conventer = new ConverterOfNumber(Int32.Parse(args[0]), Int32.Parse(args[1]));
+                Console.WriteLine($"{conventer.СonvertDeciminalNumber()}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }           
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+            }
+        }
+    }
+}
