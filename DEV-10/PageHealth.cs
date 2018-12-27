59b5d0cabfbf10c2eb19f7906486a5744eecca82
vinchevskaya <winchewskaja@gmail.com>
2018-12-27 16:11:05 +0300

    Task 10

---
+++ DEV-10/PageHealth.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+
+namespace DEV_10.Pages
+{
+    /// <summary>
+    /// Сlass to test the health of all page functions.
+    /// </summary>
+    public class PageHealth
+    {
+        private IWebElement departureStation = Browser.Driver.FindElement(By.XPath("//*[@id=\"viewns_Z7_9HD6HG80NGMO80ABJ9NPD12001_: form1:textDepStat\"]"));
+
+        private IWebElement arrivalStation = Browser.Driver.FindElement(By.XPath("//*[@id=\"viewns_Z7_9HD6HG80NGMO80ABJ9NPD12001_: form1:textArrStat\"]"));
+
+        private IWebElement departureDate = Browser.Driver.FindElement(By.XPath("///*[@id=\"viewns_Z7_9HD6HG80NGMO80ABJ9NPD12001_: form1:dob\"]"));
+
+        private IWebElement nextPage = Browser.Driver.FindElement(By.XPath("//*[@id=\"viewns_Z7_9HD6HG80NGMO80ABJ9NPD12001_: form1:buttonSearch\"]"));
+
+        /// <summary>
+        /// Go to trail page with valid data.
+        /// </summary>
+        public void MoveToNextPageWithValidData()
+        {
+            departureStation.SendKeys("МИНСК-ПАССАЖИРСКИЙ");
+            arrivalStation.SendKeys("ГОМЕЛЬ");
+            departureDate.SendKeys("16.01.2019");
+            nextPage.Click();
+        }
+
+        /// <summary>
+        /// Go to trail page when invalid data.
+        /// </summary>
+        public void MoveToNextPageWithUnValidData()
+        {
+            departureStation.SendKeys("МИНСК-ПАССАЖИРСКИЙ");
+            arrivalStation.SendKeys("СЛУЦК");
+            departureDate.SendKeys("23.12.2018");
+            nextPage.Click();
+        }
+    }
+}
---
+++ DEV-10/Pages.cs
@@ -0,0 +1,23 @@
+﻿namespace DEV_10.Pages
+{
+    public static class Pages
+    {
+        public static LoginPage LoginPage
+        {
+            get
+            {
+                var loginPage = new LoginPage();
+                return loginPage;
+            }
+        }
+
+        public static PageHealth PageHealth
+        {
+            get
+            {
+                var pageHealth = new PageHealth();
+                return pageHealth;
+            }
+        }
+    }
+}
